cc7c2258da7ac1f41ba0e153926019425ccca3c0
Gabe Banks <gabriel.t.banks@gmail.com>
2022-03-01 20:22:22 -1000

    changed loading message

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import ProgressBar from "./ProgressBar";
 export default function Loading() {
     return (
         <div className="h-screen flex flex-col items-center justify-center text-center" style={{backgroundColor: "black"}}>
-            <p style={{color: "white"}}>Loading...</p>
+            <p style={{color: "white"}}>Downloading calendar...</p>
             <ProgressBar />
         </div>
     )
