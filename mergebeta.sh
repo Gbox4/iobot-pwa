84f3282049c39272b6d0be357a1e7e88d5862fc0
Gabe Banks <gabriel.t.banks@gmail.com>
2022-03-08 19:40:25 -1000

    updated merge script

--- mergebeta.sh
+++ mergebeta.sh
@@ -1,3 +1,8 @@
-git merge --no-commit --no-ff beta
-find public | xargs -I {} git reset HEAD {}
-find public | xargs -I {} git checkout -- {}
+if [ -z "$1" ]
+then
+    echo "Please specify branch 'master' or 'beta'"
+else
+    git merge --no-commit --no-ff $1
+    find public | xargs -I {} git reset HEAD {}
+    find public | xargs -I {} git checkout -- {}
+fi
