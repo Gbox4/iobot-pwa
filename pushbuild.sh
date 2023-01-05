d13890eaecf556f22343cbb6725a6439c35c6cf2
Gabe Banks <gabe@gabebanks.net>
2023-01-05 16:26:53 -0500

    new pushbuild script

--- pushbuild.sh
+++ pushbuild.sh
@@ -1,9 +1,9 @@
 if [ "$1" = "master" ]; then
     yarn build
-    rsync -av --delete ./build/* root@gabebanks.net:/var/www/iobot/
+    rsync --rsync-path="sudo rsync" -av --delete ./build/* gabe@gabebanks.net:/var/www/iobot/
 elif [ "$1" = "beta" ]; then
     yarn build
-    rsync -av --delete ./build/* root@gabebanks.net:/var/www/betaiobot/
+    rsync --rsync-path="sudo rsync" -av --delete ./build/* gabe@gabebanks.net:/var/www/betaiobot/
 else
     echo "please specify master or beta"
 fi
